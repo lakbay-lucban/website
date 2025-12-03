fddca615cbc40194b5b9a79be88395f3438324e6
PROFILE <72250349+Profility@users.noreply.github.com>
2025-12-03 09:16:14 +0800

    navigation bar ontop
    made the navigation bar always ontop

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <div className="w-full bg-gray-900 text-white">
+        <div className="fixed top-0 left-0 w-full bg-gray-900 text-white z-50 shadow-md">
           <div className="px-4 md:px-10 py-2 flex flex-wrap justify-between">
 
             <Sheet>
