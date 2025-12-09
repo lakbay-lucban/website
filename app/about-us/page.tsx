9c68931e55dfe2330184205f55a60c73b148c5d0
PROFILE <72250349+Profility@users.noreply.github.com>
2025-12-10 07:32:34 +0800

    Update page.tsx
    made github link look better

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ShowcaseCarousel } from "@/components/showcaseCarousel";
+import { ExternalLink } from 'lucide-react';
 
 
 export default function Home() {
@@ -17,23 +18,8 @@ export default function Home() {
       <div className="w-full max-w-100 md:max-w-250 mx-auto py-10 px-4 md:text-lg text-base text-justify">
         <h1 className="font-bold text-2xl">About the Website</h1><br/>
         <p>Lakbay Lucban is a QR-code web-based information system serving as centralized guides to enhance the trip planning and visitor experience in Lucban, Quezon by offering details about destinations selected by the Tourism Office of Lucban. This website was made as the output of a research conducted by Lucban Academy Senior Highschool Grade-12 Students to modernize the tourism experience in Lucban, Quezon.</p>
-        <br/><p>This website was created using the following technologies; NextJS as the front-end, TailwindCSS for the styling, and ShadCN as component library. Other miscellaneous technologies such as Google Maps integration through embeds, GitHub for version control, and Vercel for static-site hosting. All images showcasing each destinations are available publicly in their respective Facebook Pages. The code for this website is open-sourced and is available for viewing, it can be found <a className="text-blue-700" href="https://github.com/Profility/walking-tours">here.</a></p>      
+        <br/><p>This website was created using the following technologies; NextJS as the front-end, TailwindCSS for the styling, and ShadCN as component library. Other miscellaneous technologies such as Google Maps integration through embeds, GitHub for version control, and Vercel for static-site hosting. All images showcasing each destinations are available publicly in their respective Facebook Pages. The code for this website is open-sourced and is available for viewing, it can be found <a className="text-blue-700 decoration-dotted underline" href="https://github.com/Profility/walking-tours">here{<ExternalLink className="inline-flex mx-1" size={16}/>}</a></p>      
       </div>
-      {/* <div className="w-full max-w-100 md:max-w-250 mx-auto px-4">
-        <h1 className="font-bold text-2xl">Contact Us</h1><br/>
-        <p className="text-lg text-justify">You can contact us via socials, e-mail or phone, feel free to contact us regarding questions, suggestions or feedback about the website!</p>
-        <br/><ul>
-          <li>
-            <b>Phone Number:</b> +09999999999
-          </li>
-          <li>
-            <b>Email Address:</b> placeholder@gmail.com
-          </li>
-          <li>
-            <b>Facebook:</b> Placeholder
-          </li>
-        </ul>
-      </div> */}
     </div>
   );
 }
