081a935970556dd51eed1eebfa1de73f586ab0cb
PROFILE <72250349+Profility@users.noreply.github.com>
2026-01-30 20:59:24 +0800

    Update page.tsx

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
       <div className="relative w-full">
           <ShowcaseCarousel images="/about-us.jpg" />
       
-          <h1 className="absolute top-1/2 md:left-10 transform -translate-y-1/2 text-5xl md:text-left text-center font-bold px-15 text-white text-shadow-md text-shadow-black pointer-events-none">
+          <h1 className="absolute top-1/2 md:left-10 transform -translate-y-1/2 text-5xl md:text-left text-center font-bold px-15 text-white  pointer-events-none">
           <span className="text-4xl">About Us</span>
         </h1>
       </div>
